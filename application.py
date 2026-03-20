3a332a408e6dc28ce3f9323719098294518a04e9
Abhishek Borase <abhishekborase@Abhisheks-MacBook-Air.local>
2026-03-20 11:15:14 +0530

    Implement db migration and PATCH request

--- application.py
+++ application.py
@@ -1,17 +1,20 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import datetime
 import sqlite3
 from sqlalchemy import UniqueConstraint, exc
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Student(db.Model):
     __table_args__ = (UniqueConstraint('name', 'date_of_birth', name='uix_name_date'),)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     date_of_birth = db.Column(db.Date)
+    age = db.Column(db.Integer)
 
     def __repr__(self):
         return f'{self.name} - {self.date_of_birth}'
@@ -25,19 +28,19 @@ def student():
     students = Student.query.all()
     output = []
     for student in students:
-        student_data = {'name': student.name, 'date_of_birth': student.date_of_birth.strftime("%Y-%m-%d")}
+        student_data = {'name': student.name, 'date_of_birth': student.date_of_birth.strftime("%Y-%m-%d"), 'age': student.age}
         output.append(student_data)
     return {"students" : output}
 
 @app.route('/students/<int:id>')
 def student_by_id(id):
     student = Student.query.get_or_404(id)
-    return {"name": student.name , "date_of_birth": student.date_of_birth.strftime("%Y-%m-%d")}
+    return {"name": student.name , "date_of_birth": student.date_of_birth.strftime("%Y-%m-%d"), "age": student.age}
 
 @app.route('/students', methods=['POST'])
 def add_student():
     try:
-        student = Student(name=request.json['name'], date_of_birth=datetime.datetime.strptime(request.json['date_of_birth'], "%Y-%m-%d").date())
+        student = Student(name=request.json['name'], date_of_birth=datetime.datetime.strptime(request.json['date_of_birth'], "%Y-%m-%d").date(), age=request.json['age'])
         db.session.add(student)
         db.session.commit()
     except exc.IntegrityError:
@@ -51,3 +54,12 @@ def delete_student(id):
     db.session.delete(student)
     db.session.commit()
     return {"message": "Student deleted"}
+
+@app.route('/students/<int:id>', methods=['PATCH'])
+def update_student(id):
+    student = Student.query.get_or_404(id)
+    student.name = request.json.get('name', student.name)
+    student.date_of_birth = request.json.get('date_of_birth', student.date_of_birth)
+    student.age = request.json.get('age', student.age)
+    db.session.commit()
+    return {"message": "Student updated"}
